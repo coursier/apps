88bcbc545aca1c23dc3e7fe223b715bf8c38471a
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-02-04 15:35:51 +0100

    Update .travis-setup.sh

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -10,18 +10,18 @@ mkdir -p "$DIR"
 
 export PATH="$DIR:$PATH"
 
-COURSIER_VERSION="1.1.0-M14-4"
+COURSIER_VERSION="2.0.0-RC6-1"
 MILL_VERSION="0.3.6"
 
 if [ ! -x "$DIR/coursier-$COURSIER_VERSION" ]; then
   curl -Lo "$DIR/coursier-$COURSIER_VERSION" \
-    "https://github.com/coursier/coursier/releases/download/v$COURSIER_VERSION/coursier"
+    "https://github.com/coursier/coursier/releases/download/v$COURSIER_VERSION/cs-x86_64-pc-linux"
   chmod +x "$DIR/coursier-$COURSIER_VERSION"
-  rm -f "$DIR/coursier"
-  ( cd "$DIR" && ln -s "coursier-$COURSIER_VERSION" coursier )
+  rm -f "$DIR/coursier" "$DIR/cs"
+  ( cd "$DIR" && ln -s "coursier-$COURSIER_VERSION" coursier && ln -s "coursier-$COURSIER_VERSION" cs )
 fi
 
-MILL_CLASSPATH="$(coursier fetch --classpath "com.lihaoyi:mill-dev_2.12:$MILL_VERSION")"
+MILL_CLASSPATH="$(cs fetch --classpath "com.lihaoyi:mill-dev_2.12:$MILL_VERSION")"
 coursier bootstrap -f "com.lihaoyi:mill-dev_2.12:$MILL_VERSION" \
   -o "$DIR/mill" \
   -M mill.MillMain \
