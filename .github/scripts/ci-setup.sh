coursier/apps

commit c563e016c2e371d22ad8af3b03688ea1aed5f5f6

Temporarily tweak ci-setup

.github/scripts/ci-setup.sh
@@ -23,5 +23,8 @@ fi
 
 "$DIR/cs" install --dir "$DIR" \
    "mill:$MILL_VERSION" \
-   sbt-launcher \
-   ammonite
+   sbt-launcher
+
+"$DIR/cs" bootstrap -o "$DIR/ammonite" \
+   com.lihaoyi:ammonite_2.13.2:2.1.4 \
+   -M ammonite.Main

commit c421e0bc45ca826c486c6e4fd93cf62d4a41ec93

Fix publish script
Regression in c563e016c

.github/scripts/ci-setup.sh
@@ -25,6 +25,6 @@ fi
    "mill:$MILL_VERSION" \
    sbt-launcher
 
-"$DIR/cs" bootstrap -o "$DIR/ammonite" \
+"$DIR/cs" bootstrap -o "$DIR/amm" \
    com.lihaoyi:ammonite_2.13.2:2.1.4 \
    -M ammonite.Main